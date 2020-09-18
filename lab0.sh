d3fab96c84da5d7533f942e6012602cdfefe6063
Kirill Neevin <neevin-kirill@mail.ru>
2020-09-18 22:41:10 +0600

    Create lab0.sh

---
+++ lab0.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 1
+mkdir lab0
+cd lab0
+
+mkdir dusknoir3
+cd dusknoir3
+mkdir marowak
+mkdir sealeo
+mkdir krokorok
+cd ..
+
+echo 'Ходы After You Covet Giga Drain Growthf Heal Bell Helping
+HandLeech Seed‡ Mega Drain‡ Role Play Seed bomb Sleep Talk Snore
+Synthesis‡ Worry Seed' > lilligant1
+
+mkdir musharna3
+cd musharna3
+echo 'Возможности Overland=6 Surface=4
+Jump=2 Power=l Intelligence=4 Sprouter=0' > petilil
+mkdir luxio
+echo 'Способности Last
+Chance Cute Charm Klutz' > lopunny
+mkdir gyarados
+echo 'Ходы Body Slam Covet Defense Curl
+Double-Edge Dual Chop Dynamicpunch Endeavor Fling‡ Fire Punch Focus
+Punch Gunk Shot Helping Hand Ice Punch Iron Tail Low Kick Magic Coat
+Mega Kick Mega Punch Metronome Mud-Slap Outrage Psych Up Role Play
+Seed Bomb Sleep Talk Snore Spite Swift Thunderpunch Uproar Vacuum
+Wave' > primeape
+cd ..
+
+echo 'Ходы Ancientpower Bind Body Slam Dive Double-Edge
+Earth Power Icy Wind Iron Defense Knock Off Rollout Sleep Talk Snore
+Stealth Rock' > omanyte0
+
+echo 'Способности Landslide Sand Veil
+Pickup' > sandshrew8
+
+mkdir skiploom7
+cd skiploom7
+echo 'weigth=89.3 height=51.0 atk=12 def=11' > kabutops
+echo 'Живет
+Ocean' > staryu
+echo 'satk=10 sdef=11 spd=7' > gothitelle
+echo 'Тип диеты
+Ergovore' > magnezone
+cd ..
+
+
