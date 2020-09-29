0fe7ec7671f30b4e8c15167283d0d1fb187d18ef
Kirill Neevin <neevin-kirill@mail.ru>
2020-09-29 16:53:42 +0600

    da

--- README.md
+++
@@ -1,2 +0,0 @@
-# BasicsOfProfessionalActivity
- Основы профессиональной деятельности
--- lab0.sh
+++ lab0.sh
@@ -3,6 +3,7 @@
 #REMOVING OLD VERSION OF PROJECT
 chmod -R 777 lab0
 rm -R lab0
+#rm /tmp/s313087-lab0.txt
 
 # 1
 mkdir lab0
@@ -103,3 +104,35 @@ chmod u-r lab0/skiploom7/staryu
 chmod u-r lab0/skiploom7/
 
 #4
+#chmod u+r lab0/dusknoir3/
+#cat lab0/dusknoir3/* | wc -l 1> /tmp/s313087-lab0.txt #Ошибка доступа, нельзя читать каталог dusknoir3/
+
+#chmod -R 777 lab0/
+#mkdir lab0/dusknoir3/krokorok/123
+#ls -Rl lab0/ 2> /tmp/s313087-lab0.txt | grep k$ | sort -rk2 | head -4
+
+#тут доступ к нужному файлу закрыт, поэтому ничего не выведится 
+#cat lab0/musharna3/petilil lab0/musharna3/lopunny lab0/musharna3/primeape 2> /tmp/s313087-lab0.txt | grep -i 'Pla'
+
+#echo 'aaa' > lab0/ooo
+#echo 'cbc' > lab0/o4o
+#echo 'abb' > lab0/o7o
+#echo 'ccc' > lab0/o5o
+#echo 'aab' > lab0/o6o
+#cat lab0/*o lab0/*/*o lab0/*/*/*o lab0/*/*/*/*o 2> /dev/null | sort -r
+
+#chmod -R 700 lab0/
+#cat lab0/s* lab0/*/s* lab0/*/*/s* lab0/*/*/*/s* 2> /tmp/s313087-lab0.txt | sort -r | grep -n '^'
+
+#ошибка доступа
+#chmod 700 lab0/musharna3/lopunny lab0/musharna3/primeape lab0/skiploom7/kabutops lab0/skiploom7/staryu
+#grep -h 'Me' lab0/musharna3/lopunny lab0/musharna3/primeape lab0/skiploom7/kabutops lab0/skiploom7/staryu 2>&1
+
+#5
+
+#rm lab0/omanyte0
+#rm lab0/musharna3/lopunny #cant write lopunny
+#rm lab0/Copy_*
+#rm lab0/musharna3/petilillilliga*
+#rm -r lab0/dusknoir3 # не хватает прав на удаление
+#rm -r lab0/dusknoir3/krokorok # та же фигня
